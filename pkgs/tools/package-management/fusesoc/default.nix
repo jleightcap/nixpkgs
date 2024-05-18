790a6fe7e969c825d313a8ebeee59ae9e005585a
Jack Leightcap <jack@leightcap.com>
2024-05-18 18:33:42 -0400

    fusesoc cleanup
    Signed-off-by: Jack Leightcap <jack@leightcap.com>

--- pkgs/tools/package-management/fusesoc/default.nix
+++ pkgs/tools/package-management/fusesoc/default.nix
@@ -11,29 +11,42 @@
 , simplesat
 , ipyxact
 , setuptools-scm
+, pytestCheckHook
 }:
 buildPythonPackage rec {
   pname = "fusesoc";
-  version = "2.2.1";
+  version = "2.3";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-M36bXBgY8hR33AVDlHoH8PZJG2Bi0KOEI07IMns7R4w=";
+    hash = "sha256-ruYx9dDAm23jV4tw1Op+6Pe1ea1c7VWH5RzqasFsZ6E=";
   };
 
   nativeBuildInputs = [ setuptools-scm ];
 
-  propagatedBuildInputs = [ edalize fastjsonschema pyparsing pyyaml simplesat ipyxact ];
+  propagatedBuildInputs = [ edalize fastjsonschema pyparsing pyyaml simplesat ];
 
   pythonImportsCheck = [ "fusesoc" ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  disabledTests = [
+    # executes git clone
+    "test_library_update"
+    # touches network
+    "test_git_provider"
+    "test_github_provider"
+    "test_url_provider"
+  ];
 
   makeWrapperArgs = [ "--suffix PATH : ${lib.makeBinPath [ iverilog verilator gnumake ]}"];
 
   meta = with lib; {
+    changelog = "https://github.com/olofk/fusesoc/releases/tag/${version}";
     homepage = "https://github.com/olofk/fusesoc";
-    description = "Package manager and build tools for HDL code";
-    maintainers = with maintainers; [ genericnerdyusername ];
-    license = licenses.bsd3;
+    description = "Package manager and build abstraction tool for FPGA/ASIC development";
+    maintainers = with maintainers; [ genericnerdyusername jleightcap ];
+    license = licenses.bsd2;
     mainProgram = "fusesoc";
   };
 }
