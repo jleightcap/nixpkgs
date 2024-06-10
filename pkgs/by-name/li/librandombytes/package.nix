5a1ca4b5e12d202b645eba7d5f1d3789703d3eeb
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2024-06-10 15:39:35 +0200

    Add some TODOs

--- pkgs/by-name/li/librandombytes/package.nix
+++ pkgs/by-name/li/librandombytes/package.nix
@@ -13,9 +13,13 @@ stdenv.mkDerivation (prev: {
     url = "https://randombytes.cr.yp.to/librandombytes-${prev.version}.tar.gz";
     hash = "sha256-LE8iWw7FxckPREyqefgKtslD6CPDsL7VsfHScQ6JmLs=";
   };
-
+  # TODO: Why is not stripped?
+  # TODO: why do we need Qunused-arguments for clang?
+  # TODO: build with clang
   env.NIX_CFLAGS_COMPILE = toString (lib.optionals stdenv.cc.isClang [ "-Qunused-arguments" ]);
 
+  # TODO: submit to upstream
+  # TODO: clean up
   patches = [ ./cross.patch ];
 
   nativeBuildInputs = [ python3 ];
@@ -27,5 +31,7 @@ stdenv.mkDerivation (prev: {
     patchShebangs scripts-build
   '';
 
+  # TODO: this should be a variable, if not present, we get:
+  # ValueError: unrecognized argument --build=x86_64-unknown-linux-gnu
   configurePlatforms = [ "aarch64" ];
 })
