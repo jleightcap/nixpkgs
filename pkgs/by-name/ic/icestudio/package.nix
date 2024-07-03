41daf23fd2a37c7f288eb297692c503202abc115
Enric Morales <me@enric.me>
2024-07-03 18:40:19 +0200

    troubleshoot nw

--- pkgs/by-name/ic/icestudio/package.nix
+++ pkgs/by-name/ic/icestudio/package.nix
@@ -1,9 +1,12 @@
-{ lib
-, stdenv
-, fetchFromGitHub
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  buildNpmPackage,
+  nwjs,
 }:
 
-stdenv.mkDerivation rec {
+buildNpmPackage rec {
   pname = "icestudio";
   version = "0.12";
 
@@ -13,6 +16,26 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     hash = "sha256-LjkWje7u7IG9gMrXi6nfHXzD8ZIxm+gybh5+/Sn4PkY=";
   };
+
+  preFixup = ''
+    patchShebangs scripts/postInstall.sh
+  '';
+
+  postPatch = ''
+    cp ${./package-lock.json} package-lock.json
+    cp ${./package.json} package.json
+  '';
+
+  npmDepsHash = "sha256-VG1vi5PDzcGjChnsZzoNlNDTLmcAN4LpHuPVmmMWavo=";
+
+  npmFlags = [ "--legacy-peer-deps" ];
+
+  dontNpmBuild = true;
+
+  installPhase = ''
+    npm run buildLinux64
+    cp -rv dist/icestudio/linux64/* $out
+  '';
 
   meta = with lib; {
     description = "Snowflake: Visual editor for open FPGA boards";
