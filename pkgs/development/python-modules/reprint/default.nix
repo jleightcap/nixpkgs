da39c87e1eba9ea457348291ba3fe0ceeb924715
Jason Odoom <jasonodoom@gmail.com>
2024-08-20 01:57:20 -0400

    python3Packages.reprint: init at 0.6.0
    python3Packages.reprint: init at 0.6.0
    
    python3Packages.reprint: init at 0.6.0

---
+++ pkgs/development/python-modules/reprint/default.nix
@@ -0,0 +1,41 @@
+{
+  buildPythonPackage,
+  colorama,
+  fetchFromGitHub,
+  lib,
+  setuptools,
+  six,
+  stdenv,
+}:
+
+buildPythonPackage rec {
+  pname = "reprint";
+  version = "0.6.0";
+
+  src = fetchFromGitHub {
+    owner = "Yinzo";
+    repo = "reprint";
+    rev = "${version}";
+    hash = "sha256-99FC12LcvvRRwNAxDSvWo9vRYmieL0JHSaCJqO/UGEs=";
+  };
+  doCheck = false; # Disable tests due to unnecessary dependency on backports.shutil_get_terminal_size
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    colorama
+    six
+  ];
+
+  pythonImportsCheck = [ "reprint" ];
+
+  meta = {
+    description = "Module for binding variables and refreshing multi-line output in terminal";
+    homepage = "https://github.com/Yinzo/reprint";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [
+      jasonodoom
+      jleightcap
+    ];
+  };
+}
